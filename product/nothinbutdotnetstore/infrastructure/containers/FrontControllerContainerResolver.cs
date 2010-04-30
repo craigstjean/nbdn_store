062b5c733f1ed08de4223bde5bea48903a3f6606
Craig St. Jean <craigstjean@gmail.com>
2010-04-30 11:50:34 -0600

    Changing lifecycle to singletons

--- product/nothinbutdotnetstore/infrastructure/containers/FrontControllerContainerResolver.cs
+++ product/nothinbutdotnetstore/infrastructure/containers/FrontControllerContainerResolver.cs
@@ -5,15 +5,17 @@ namespace nothinbutdotnetstore.infrastructure.containers
 	public class FrontControllerContainerResolver : ContainerResolver
 	{
 		CommandRegistry command_registry;
+		FrontController front_controller;
 
 		public FrontControllerContainerResolver(CommandRegistry command_registry)
 		{
 			this.command_registry = command_registry;
+			front_controller = new DefaultFrontController(command_registry);
 		}
 
 		public object resolve()
 		{
-			return new DefaultFrontController(command_registry);
+			return front_controller;
 		}
 	}
 }
--- product/nothinbutdotnetstore/infrastructure/containers/RequestFactoryContainerResolver.cs
+++ product/nothinbutdotnetstore/infrastructure/containers/RequestFactoryContainerResolver.cs
@@ -6,15 +6,17 @@ namespace nothinbutdotnetstore.infrastructure.containers
 	public class RequestFactoryContainerResolver : ContainerResolver
 	{
 		MapperRegistry mapper_registry;
+		RequestFactory request_factory;
 
 		public RequestFactoryContainerResolver(MapperRegistry mapper_registry)
 		{
 			this.mapper_registry = mapper_registry;
+			request_factory = new DefaultRequestFactory(mapper_registry);
 		}
 
 		public object resolve()
 		{
-			return new DefaultRequestFactory(mapper_registry);
+			return request_factory;
 		}
 	}
 }
