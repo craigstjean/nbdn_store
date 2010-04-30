ef2d9f8d9eb9c34d24e7c8967a7f6b41d4bf1ba7
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-04-29 20:26:47 -0600

    Removed obsolete files

---
+++ product/nothinbutdotnetstore/tasks/Startup.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace nothinbutdotnetstore.tasks
+{
+    public class Startup
+    {
+        public static void run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
